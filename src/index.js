b05c043acaca36d7f4d37eb6dfb04336b67e6c79
Xiyang Chen <settinghead@gmail.com>
2016-10-27 23:48:38 -0400

    predicate own index

--- src/index.js
+++ src/index.js
@@ -4,18 +4,7 @@ var namespaceFn = require('./namespace');
 var ops = require('./ops');
 var utils = require('./utils');
 
-var predicates = {
-  isBool: require('./preds/isBool'),
-  isBoolean: require('./preds/isBool'),
-  isFn: require('./preds/isFn'),
-  isFunction: require('./preds/isFn'),
-  isNum: require('./preds/isNum'),
-  isObj: require('./preds/isObj'),
-  isObject: require('./preds/isObj'),
-  isStr: require('./preds/isStr'),
-  isString: require('./preds/isStr'),
-  isUndefined: require('./preds/isUndefined'),
-};
+var predicates = require('./preds');
 
 var models = {
   Problem: require('./models/Problem'),
---
+++ src/preds/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  isBool: require('./isBool'),
+  isBoolean: require('./isBool'),
+  isFn: require('./isFn'),
+  isFunction: require('./isFn'),
+  isNum: require('./isNum'),
+  isObj: require('./isObj'),
+  isObject: require('./isObj'),
+  isStr: require('./isStr'),
+  isString: require('./isStr'),
+  isUndefined: require('./isUndefined'),
+  notEmpty: require('./notEmpty'),
+};
