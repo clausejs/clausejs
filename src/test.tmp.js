a5e54a5ae696deb3b3a67673fd3b177464e69c49
Xiyang Chen <settinghead@gmail.com>
2016-10-27 23:49:35 -0400

    revised composite namespace

--- src/test.tmp.js
+++ src/test.tmp.js
@@ -16,31 +16,43 @@ console.log(s);
 // var r = s('xyz.superapp.item');
 // console.log(r.conform({z:1}));
 
-s('xyz.superapp.item', s.and(s.isObj, s.props({req: ['title', 'content']})));
-s('xyz.superapp.item.title', s.isStr);
-var r = s('xyz.superapp.item').conform({ title: 'a', content: 'b' });
-console.log(r);
+// s('xyz.superapp.item', s.and(s.isObj, s.props({req: ['title', 'content']})));
+// s('xyz.superapp.item.title', s.isStr);
+// var r = s('xyz.superapp.item').conform({ title: 'a', content: 'b' });
+// console.log(r);
 
-// s('todoapp', {
-//   'headline': s.isStr,
-//   'list': {
-//     'title': s('../headline'),
-//     'items': s.zeroOrMore(s('../item')),
-//   },
-//   // 'item': [
-//   // s.props({
-//   //   req: [s('title'), s('content'), s('date'), s('isDone')],
-//   //   opt: [s('reminder')],
-//   // }),
-//   // s('title', s('../headline')),
-//   // s('content', s.and(s.isStr, s.notEmpty)),
-//   // s('date', s.isDate),
-//   // s('isDone', s.isBool),
-//   // s('reminder', s.isDate)],
-// });
+s('todoapp',
+  s('headline', s.and(s.isStr, s.notEmpty)),
+  {
+    'list': s.props({
+      req: {
+        'title': s('todoapp.headline'),
+        'items': s.zeroOrMore(s('todoapp.item')),
+      },
+    }),
+    'item': [
+      s.props({
+        req: {
+          title: s('./title'),
+          content: s('./content'),
+          isDone: s('./isDone'),
+          reminder: s('./reminder'),
+        },
+        opt: {
+          reminder: s.isDate,
+        },
+      }),
+      s('title', s('todoapp.headline')),
+      s('content', s.and(s.isStr, s.notEmpty)),
+      s('date', s.isDate),
+      s('isDone', s.isBool),
+      s('reminder', s.isDate)],
+});
 
-// var ListSpec = s('todoapp.list');
+
+var ListSpec = s('todoapp.list');
 // var ItemSpec = s('todoapp.item');
 // var contentSpec = s('todoapp.item.content');
 //
-// console.log(ListSpec);
+var r = ListSpec.conform({ title: 'hello', items: [] });
+console.log(r);
