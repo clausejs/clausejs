edf1b62622d01a33520d65cd4fb6d99b701a7e1a
Xiyang Chen <settinghead@gmail.com>
2016-12-04 22:35:14 -0500

    specref inherits spec

--- src/models/SpecRef.js
+++ src/models/SpecRef.js
@@ -1,8 +1,12 @@
+import Spec from './Spec';
+
 function SpecRef( { ref, getFn, conformFn } ) {
   this.type = 'SpecRef';
   this.get = getFn;
   this.conform = conformFn;
   this.ref = ref;
 }
+
+SpecRef.prototype = Object.create( Spec.prototype );
 
 module.exports = SpecRef;
