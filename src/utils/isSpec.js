130156345df075e83584a143a94f4a15a142f129
Xiyang Chen <settinghead@gmail.com>
2016-12-08 21:17:47 -0500

    simplify isSpec

--- src/utils/isSpec.js
+++ src/utils/isSpec.js
@@ -1,11 +1,7 @@
 var Spec = require( '../models/Spec' );
 
 function isSpec( x ) {
-  if ( !x ) {
-    return false;
-  } else {
-    return x instanceof Spec;
-  }
+  return x instanceof Spec;
 }
 
 module.exports = isSpec;
