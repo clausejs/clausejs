8edc6f4bb9408966fba38585c409d814acd8e1c6
Xiyang Chen <settinghead@gmail.com>
2016-12-08 21:17:38 -0500

    function names in delayed spec

--- src/models/DelayedSpec.js
+++ src/models/DelayedSpec.js
@@ -5,12 +5,20 @@ function DelayedSpec( { getFn } ) {
   this.get = getFn;
   var _this = this;
 
-  [ 'conform', 'instrument' ].forEach( ( mName ) => {
-    _this[ mName ] = ( x ) => {
-      var Spec = getFn();
-      return Spec[ mName ].call( this, x );
-    };
-  } );
+  _this.instrument = function instrumentDelayed( x ) {
+    var Spec = getFn();
+    return Spec.instrument( x );
+  };
+
+  _this.instrumentConformed = function instrumentConformedDelayed( x ) {
+    var Spec = getFn();
+    return Spec.instrumentConformed( x );
+  };
+
+  _this.conform = function conformDelayed( x ) {
+    var Spec = getFn();
+    return Spec.conform( x );
+  };
 }
 
 DelayedSpec.prototype = Object.create( Spec.prototype );
