910b8a5df3bed8be1fd0156b0d533f87e11a0fc2
Xiyang Chen <settinghead@gmail.com>
2016-12-12 19:33:52 -0500

    dictate package version from a js file

--- sync-all-package-versions.js
+++ sync-all-package-versions.js
@@ -2,21 +2,25 @@
 var fs = require( 'fs' );
 var path = require( 'path' );
 
-var rootObj = JSON.parse( fs.readFileSync( path.join( __dirname, 'package.json' ) ) );
+var VERSION = require( './package_version' );
+var rootPackageFilePath = path.join( __dirname, 'package.json' );
+var rootObj = JSON.parse( fs.readFileSync( rootPackageFilePath ) );
+rootObj.version = VERSION;
+fs.writeFileSync( rootPackageFilePath, JSON.stringify( rootObj, null, 2 ) );
 
 fs.readdir( path.join( __dirname, 'packages' ), ( err, files ) => {
   files.forEach( file => {
     var packagePath = path.join( __dirname, 'packages', file );
     if ( fs.lstatSync( packagePath ).isDirectory() ) {
-      var packagePath = path.join( packagePath, 'package.json' );
-      var obj = JSON.parse( fs.readFileSync( packagePath, 'utf8' ) );
+      var packageFile = path.join( packagePath, 'package.json' );
+      var obj = JSON.parse( fs.readFileSync( packageFile, 'utf8' ) );
       // sync package versions
-      obj.version = rootObj.version;
+      obj.version = VERSION;
 
       // sync peer dependency version
       updateDeps_mut( obj.peerDependencies );
       updateDeps_mut( obj.dependencies );
-      fs.writeFileSync( packagePath, JSON.stringify( obj, null, 2 ) );
+      fs.writeFileSync( packageFile, JSON.stringify( obj, null, 2 ) );
     }
   } );
 } );
@@ -25,8 +29,10 @@ function updateDeps_mut( deps ) {
   if ( deps ) {
     for ( var depName in deps ) {
       if ( deps.hasOwnProperty( depName ) && /^specky/.test( depName ) ) {
-        deps[ depName ] = rootObj.version;
+        deps[ depName ] = VERSION;
       }
     }
   }
 }
+
+console.log( 'All versions set to ', VERSION );
