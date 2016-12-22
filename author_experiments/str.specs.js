dd8150f80a95682b9655f1dd35d6eb6b549fed8b
Xiyang Chen <settinghead@gmail.com>
2016-12-22 18:51:52 -0500

    str conform experiment

---
+++ author_experiments/str.specs.js
@@ -0,0 +1,14 @@
+require( 'babel-core/register' );
+
+var S = require( '../src' );
+
+var catS = function( str ) {
+  return S.cat.apply( null, Array.from( str ).map( S.equals ) );
+};
+
+var VocabSpec = S.or.apply( null, [ 'foo', 'bar', 'baz', ' ' ].map( catS ) );
+var ContentSpec = S.zeroOrMore( VocabSpec );
+
+var r = ContentSpec.conform( ' baz foo bar bar' );
+
+console.log( r );
