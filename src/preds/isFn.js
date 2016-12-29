7182d7d2ee346f13032e427efc53dcc9b0b0b450
Xiyang Chen <settinghead@gmail.com>
2016-12-29 18:51:33 -0500

    isFn handles arrow functions

--- src/preds/isFn.js
+++ src/preds/isFn.js
@@ -1,7 +1,1 @@
-function isFunction( x ) {
-  var getType = {};
-  // (x || false) guarantees returning of boolean type
-  return ( x || false ) && getType.toString.call( x ) === '[object Function]';
-}
-
-module.exports = isFunction;
+module.exports = require( 'is-function-x' );
